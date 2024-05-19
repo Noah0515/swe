46fd01daa06cdcfab7933894766446845e82324f
CS Yoon <01csyoon@gmail.com>
2024-05-19 22:56:56 +0900

    찐찐막

--- src/main/java/API/KakaoApi.java
+++ src/main/java/API/KakaoApi.java
@@ -1,11 +1,13 @@
 package API;
 
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 
 @Component
+@Getter
 public class KakaoApi {
     @Value("${kakao.api_key}")
     private String kakaoApiKey;
