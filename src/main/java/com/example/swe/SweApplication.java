0bde8b83f8fa927a53027f13bd61f3fa55678751
JOO <zpffjtm@naver.com>
2024-05-19 22:08:26 +0900

    recent

--- src/main/java/com/example/swe/SweApplication.java
+++ src/main/java/com/example/swe/SweApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DineOpsApplication {
+public class SweApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DineOpsApplication.class, args);
+		SpringApplication.run(SweApplication.class, args);
 	}
 
 }
