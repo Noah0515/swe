c048a476f6439b15a25a31044d25fd6f0711f26b
JOO <zpffjtm@naver.com>
2024-05-19 13:03:53 +0900

    last

--- src/main/java/DTO/FoodDTO.java
+++ src/main/java/DTO/FoodDTO.java
@@ -1,5 +1,123 @@
 package DTO;
 
 public class FoodDTO {
-    
+    private int foodQuantity;
+    private String foodName;
+    private String foodCategory;
+    private String expirationDate;
+    private double foodPrice;
+    private String purchaseDate;
+    private String supplier;
+    private boolean isDefective;
+    private String allergens;
+    private String nutrients;
+    private int usagePeriod;
+    private String storageLocation;
+    private Long restaurantId;
+
+    // Getters and Setters
+    public int getFoodQuantity() {
+        return foodQuantity;
+    }
+
+    public void setFoodQuantity(int foodQuantity) {
+        this.foodQuantity = foodQuantity;
+    }
+
+    public String getFoodName() {
+        return foodName;
+    }
+
+    public void setFoodName(String foodName) {
+        this.foodName = foodName;
+    }
+
+    public String getFoodCategory() {
+        return foodCategory;
+    }
+
+    public void setFoodCategory(String foodCategory) {
+        this.foodCategory = foodCategory;
+    }
+
+    public String getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(String expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public double getFoodPrice() {
+        return foodPrice;
+    }
+
+    public void setFoodPrice(double foodPrice) {
+        this.foodPrice = foodPrice;
+    }
+
+    public String getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setPurchaseDate(String purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+
+    public String getSupplier() {
+        return supplier;
+    }
+
+    public void setSupplier(String supplier) {
+        this.supplier = supplier;
+    }
+
+    public boolean isDefective() {
+        return isDefective;
+    }
+
+    public void setDefective(boolean defective) {
+        isDefective = defective;
+    }
+
+    public String getAllergens() {
+        return allergens;
+    }
+
+    public void setAllergens(String allergens) {
+        this.allergens = allergens;
+    }
+
+    public String getNutrients() {
+        return nutrients;
+    }
+
+    public void setNutrients(String nutrients) {
+        this.nutrients = nutrients;
+    }
+
+    public int getUsagePeriod() {
+        return usagePeriod;
+    }
+
+    public void setUsagePeriod(int usagePeriod) {
+        this.usagePeriod = usagePeriod;
+    }
+
+    public String getStorageLocation() {
+        return storageLocation;
+    }
+
+    public void setStorageLocation(String storageLocation) {
+        this.storageLocation = storageLocation;
+    }
+
+    public Long getRestaurantId() {
+        return restaurantId;
+    }
+
+    public void setRestaurantId(Long restaurantId) {
+        this.restaurantId = restaurantId;
+    }
 }
+
